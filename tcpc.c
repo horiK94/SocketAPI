5051cb17b24a08025503e0a25e6e88e61c6a16e2
堀江 一樹 <s01722@CG2890.local>
2018-05-01 18:24:51 +0900

    コマンドライン上で文字の受け渡しができるように変更

--- tcpc.c
+++ tcpc.c
@@ -1,12 +1,11 @@
 #include <stdio.h> //printf(), fprintf(), perror()
-#include <sys/socket.h> //socket(), connect(), recv()
+#include <sys/socket.h> //socket(), bind(), accept(), listen()
 #include <arpa/inet.h> // struct sockaddr_in, struct sockaddr, inet_ntoa(), inet_aton()
 #include <stdlib.h> //atoi(), exit(), EXIT_FAILURE, EXIT_SUCCESS
-#include <string.h> //memset()
+#include <string.h> //memset(), strcmp()
 #include <unistd.h> //close()
 
-#define MSGSIZE 32
-#define MAX_MSGSIZE 1024
+#define MSGSIZE 1024
 #define BUFSIZE (MSGSIZE + 1)
 
 int main(int argc, char* argv[]) {
@@ -14,8 +13,8 @@ int main(int argc, char* argv[]) {
     int sock; //local socket descripter
     struct sockaddr_in servSockAddr; //server internet socket address
     unsigned short servPort; //server port number
-    char recvBuffer[BUFSIZE]; //receive temporary buffer
-    int byteRcvd, totalBytesRcvd; //received buffer size
+    char recvBuffer[BUFSIZE];//receive temporary buffer
+    char sendBuffer[BUFSIZE]; // send temporary buffer
 
     if (argc != 3) {
         fprintf(stderr, "argument count mismatch error.\n");
@@ -49,23 +48,43 @@ int main(int argc, char* argv[]) {
 
     printf("connect to %s\n", inet_ntoa(servSockAddr.sin_addr));
 
-    totalBytesRcvd = 0;
-    while (totalBytesRcvd < MAX_MSGSIZE) {
-        if ((byteRcvd = recv(sock, recvBuffer, MSGSIZE, 0)) > 0) {
-            recvBuffer[byteRcvd] = '\0';
-            printf("%s", recvBuffer);
-            totalBytesRcvd += byteRcvd;
-        } else if(byteRcvd == 0){
-            perror("ERR_EMPTY_RESPONSE");
-            exit(EXIT_FAILURE);
-        } else {
-            perror("recv() failed.");
+    while(1){
+        printf("please enter the characters:");
+        if (fgets(sendBuffer, BUFSIZE, stdin) == NULL){
+            fprintf(stderr, "invalid input string.\n");
             exit(EXIT_FAILURE);
         }
-    }
-    printf("\n");
 
-    close(sock);
+        if (send(sock, sendBuffer, strlen(sendBuffer), 0) <= 0) {
+            perror("send() failed.");
+            exit(EXIT_FAILURE);
+        }
+
+        int byteRcvd  = 0;
+        int byteIndex = 0;
+        while (byteIndex < MSGSIZE) {
+            byteRcvd = recv(sock, &recvBuffer[byteIndex], 1, 0);
+            if (byteRcvd > 0) {
+                if (recvBuffer[byteIndex] == '\n'){
+                    recvBuffer[byteIndex] = '\0';
+                    if (strcmp(recvBuffer, "quit") == 0) {
+                        close(sock);
+                        return EXIT_SUCCESS;
+                    } else {
+                        break;
+                    }
+                }
+                byteIndex += byteRcvd;
+            } else if(byteRcvd == 0){
+                perror("ERR_EMPTY_RESPONSE");
+                exit(EXIT_FAILURE);
+            } else {
+                perror("recv() failed.");
+                exit(EXIT_FAILURE);
+            }
+        }
+        printf("server return: %s\n", recvBuffer);
+    }
 
     return EXIT_SUCCESS;
 }
